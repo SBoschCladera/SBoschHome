7c7412a0e05cfd736fe85eecd85cb14e5a9c23ee
“SBoschCladeragit config --global user.email sebastianboschcladera@gmail.comexitgit config --global user.name “nom_alumne”git --versiongit config --global user.name “SBoschCladera <sebastianboschcladera@gmail.com>
2021-10-18 16:11:10 +0200

    16:00

---
+++ Arrays/world_mapping.php
@@ -0,0 +1,29 @@
+<?php
+
+$contents_cities = file_get_contents("https://dawsonferrer.com/allabres/apis_solutions/world.php?data=cities");
+$cities = json_decode($contents_cities, true);
+$contents_countries = file_get_contents("https://dawsonferrer.com/allabres/apis_solutions/world.php?data=countries");
+$countries = json_decode($contents_countries, true);
+
+function mapCities()
+{
+    //TODO: Your code here
+
+    global $cities;
+    global $countries
+
+    for($i =0; $i < count($cities);$i++){
+
+    }
+
+
+
+}
+
+function mapCountries()
+{
+    //TODO: Your code here
+}
+
+var_dump(mapCities());
+var_dump(mapCountries());
