1a348923f3a6e911447216bd715de2e98b74a356
“SBoschCladeragit config --global user.email sebastianboschcladera@gmail.comexitgit config --global user.name “nom_alumne”git --versiongit config --global user.name “SBoschCladera <sebastianboschcladera@gmail.com>
2021-10-26 20:55:43 +0200

    proyecto

---
+++ OPP/character.php
@@ -0,0 +1,230 @@
+<?php
+
+class character
+{
+
+    private $id;
+    private $name;
+    private $status;
+    private $species;
+    private $type;
+    private $gender;
+    private $origin;
+    private $location;
+    private $image;
+    private $created;
+    private $episodes = array();
+
+public function __construct($id, $name,$status, $species, $type, $gender, $origin, $location, $image, $created, array $episodes)
+{
+    $this->id = $id;
+    $this->name = $name;
+    $this->status = $status;
+    $this->species = $species;
+    $this->type = $type;
+    $this->gender = $gender;
+    $this->origin = $origin;
+    $this->location = $location;
+    $this->image = $image;
+    $this->created = $created;
+    $this->episodes = $episodes;
+}
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return characters
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     * @return characters
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     * @return characters
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSpecies()
+    {
+        return $this->species;
+    }
+
+    /**
+     * @param mixed $species
+     * @return characters
+     */
+    public function setSpecies($species)
+    {
+        $this->species = $species;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param mixed $type
+     * @return characters
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param mixed $gender
+     * @return characters
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOrigin()
+    {
+        return $this->origin;
+    }
+
+    /**
+     * @param mixed $origin
+     * @return characters
+     */
+    public function setOrigin($origin)
+    {
+        $this->origin = $origin;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param mixed $location
+     * @return characters
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param mixed $image
+     * @return characters
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param mixed $created
+     * @return characters
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getEpisodes(): array
+    {
+        return $this->episodes;
+    }
+
+    /**
+     * @param array $episodes
+     * @return characters
+     */
+    public function setEpisodes(array $episodes): characters
+    {
+        $this->episodes = $episodes;
+        return $this;
+    }
+}
---
+++ OPP/episode.php
@@ -0,0 +1,129 @@
+<?php
+
+class episode
+{
+    private $id;
+    private $name;
+    private $air_date;
+    private $episode;
+    private $created;
+    private $characters = array();
+
+    public function __construct($id, $name, $air_date, $episode, $created, array $characters)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->air_date = $air_date;
+        $this->episode = $episode;
+        $this->created = $created;
+        $this->characters = $characters;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return episode
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     * @return episode
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAirDate()
+    {
+        return $this->air_date;
+    }
+
+    /**
+     * @param mixed $air_date
+     * @return episode
+     */
+    public function setAirDate($air_date)
+    {
+        $this->air_date = $air_date;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEpisode()
+    {
+        return $this->episode;
+    }
+
+    /**
+     * @param mixed $episode
+     * @return episode
+     */
+    public function setEpisode($episode)
+    {
+        $this->episode = $episode;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param mixed $created
+     * @return episode
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCharacters(): array
+    {
+        return $this->characters;
+    }
+
+    /**
+     * @param array $characters
+     * @return episode
+     */
+    public function setCharacters(array $characters): episode
+    {
+        $this->characters = $characters;
+        return $this;
+    }
+}
