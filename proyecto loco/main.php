6b9468e340bbdb739424bd5064c9c7d2174afbcc
SBoschCladera <sebastianboschcladera@gmail.com>
2021-10-27 10:10:01 +0200

    Todo Ok

---
+++ proyecto loco/main.php
@@ -0,0 +1,37 @@
+<?php
+
+include circunscripciones;
+include partido;
+include provincia;
+
+
+$provincias = ["M" => "Madrid", "B" => "Barcelona","V" => "Valencia","A" => "Alicante","S" => "Sevilla","MA" => "Málaga","MU" => "Murcia",
+    "CA" => "Cádiz","PM" => "Baleares","C" => "La Coruña","GC" => "Las Palmas", "BI" => "Vizcaya","O" => "Asturias", "GR" => "Granada",
+    "PO" =>  "Pontevedra", "TF" => "Santa Cruz de Tenerife" ,"Z" => "Zaragoza" ,"AL" => "Almería", "BA" => "Badajoz","CO" => "Córdoba",
+    "GI" => "Gerona", "SS" => "Guipúzcoa", "T" => "Tarragona","TO" => "Toledo","S" => "Cantabria", "CS" => "Castellón", "CR" => "Ciudad Real",
+    "H" => "Huelva", "J" => "Jaén", "NA" => "Navarra",    "VA" =>  "Valladolid","VI" => "Álava", "AB" => "Albacete", "BU" => "Burgos",
+    "CC" => "Cáceres", "LE" => "León", "L" => "Lérida", "LU" => "Lugo", "OR" => "Orense", "LO" => "La Rioja", "SA" => "Salamanca", "AV" =>  "Ávila",
+    "CU" => "Cuenca","GU" =>  "Guadalajara", "HU" => "Huesca", "P" => "Palencia", "SG" => "Segovia", "TE" => "Teruel", "ZA" => "Zamora","SO" => "Soria",
+    "CE" => "Ceuta","ML" => "Melilla"];
+
+
+
+$circunscripciones = ["Madrid" => "37", "Barcelona"=> "32","Valencia"=> "15","Alicante"=> "12","Sevilla"=> "12","Málaga"=> "11",
+    "Murcia"=> "10","Cádiz"=> "9","Baleares"=> "8","La Coruña"=> "8", "Las Palmas"=> "8", "Vizcaya"=> "8","Asturias"=> "7",
+    "Granada"=> "7", "Pontevedra"=> "7", "Santa Cruz de Tenerife"=> "7" ,"Zaragoza" => "7","Almería"=> "6", "Badajoz"=> "6",
+    "Córdoba"=> "6", "Gerona"=> "6", "Guipúzcoa"=> "6", "Tarragona"=> "6","Toledo"=> "6","Cantabria"=> "5", "Castellón"=> "5",
+    "Ciudad Real"=> "5", "Huelva"=> "5", "Jaén"=> "5", "Navarra" => "5","Valladolid"=> "5","Álava"=> "4", "Albacete"=> "4",
+    "Burgos"=> "4", "Cáceres"=> "4", "León"=> "4", "Lérida"=> "4", "Lugo"=> "4", "Orense"=> "4", "La Rioja"=> "4",
+    "Salamanca"=> "4", "Ávila"=> "3", "Cuenca"=> "3", "Guadalajara"=> "3", "Huesca"=> "3", "Palencia"=> "3", "Segovia"=> "3",
+    "Teruel"=> "3", "Zamora"=> "3","Soria"=> "2","Ceuta"=> "1","Melilla"=> "1"];
+
+$votos = ["Madrid" => "3591464", "Barcelona"=> "2942354","Valencia"=> "1402405","Alicante"=> "860050","Sevilla"=> "1071247","Málaga"=> "766819",
+    "Murcia"=> "722345","Cádiz"=> "624195","Baleares"=> "458592","La Coruña"=> "620550", "Las Palmas"=> "489354", "Vizcaya"=> "633977","Asturias"=> "565231",
+    "Granada"=> "490902", "Pontevedra"=> "530077", "Santa Cruz de Tenerife"=> "468022" ,"Zaragoza" => "517639","Almería"=> "305414", "Badajoz"=> "376940",
+    "Córdoba"=> "451775", "Gerona"=> "357801", "Guipúzcoa"=> "384546", "Tarragona"=> "388088","Toledo"=> "368619","Cantabria"=> "329821", "Castellón"=> "297492",
+    "Ciudad Real"=> "272470", "Huelva"=> "255329", "Jaén"=> "371043", "Navarra" => "337996","Valladolid"=> "314787","Álava"=> "172199", "Albacete"=> "216917",
+    "Burgos"=> "201664", "Cáceres"=> "227621", "León"=> "265711", "Lérida"=> "4", "Lugo"=> "183141", "Orense"=> "173598", "La Rioja"=> "167549","Salamanca"=> "197673",
+    "Ávila"=> "94793", "Cuenca"=> "110537", "Guadalajara"=> "133796", "Huesca"=> "114791", "Palencia"=> "97038", "Segovia"=> "86451",
+    "Teruel"=> "74661", "Zamora"=> "101334","Soria"=> "47248","Ceuta"=> "33746","Melilla"=> "31172"];
+
+$partidos = ["PP", "PSOE", "Podemos", "Ciudadanos",  "ERC", "IU", "VOX", "PNV", "Més per Mallorca"];
